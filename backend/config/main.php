9e4b3d1cb946b593a3f636388dcef8ef3ee3e402
Your Name <you@example.com>
2017-02-25 10:36:32 -0800

    v0.7.1 (backend:admin config theme pathMap)

--- backend/config/main.php
+++ backend/config/main.php
@@ -61,8 +61,8 @@ return [
         'view' => [
              'theme' => [
                  'pathMap' => [ ///?????'pathMap' is empty : $pathMap = [Yii::$app->getBasePath() => [$basePath]];
-                    '@backend/views' => '@vendor/yongtiger/yii2-admin/src/views',   ///[yii2-admin release version 1.2.0 (Layout:main, main-login)]
-                    // '@backend/views' => '@vendor/yongtiger/yii2-adminlte-asset/src/views',   ///[yii2-adminlte-asset]
+                    // '@backend/views' => '@vendor/yongtiger/admin/views',   ///[yii2-admin release version 1.2.0 (Layout:main, main-login)]
+                    '@backend/views' => '@yongtiger/adminlteasset/views',   ///[yii2-adminlte-asset]
                     // ...
                  ],
              ],
