af122dd0f99789f2f320fb5bea05bfb320c15002
Your Name <you@example.com>
2017-03-14 21:16:35 -0700

    v0.16.0 (#ADD backend:isAdminEnd)

--- backend/components/Application.php
+++ backend/components/Application.php
@@ -35,7 +35,7 @@ class Application extends \yongtiger\application\Application
 
         ///[v0.10.5 (filter theme bootstrap)]
         ///You can still run without yii2 extension `yongtiger/yii-theme`.
-        ///Note: Cannot use `class_exists('yongtiger\\theme\\Bootstrap')` before application init!
+        ///Note: Cannot use `class_exists('yongtiger\\theme\\Bootstrap')` before application init()!
         if (is_file($this->getVendorPath() . DIRECTORY_SEPARATOR . 'yongtiger'. DIRECTORY_SEPARATOR . 'yii2-theme' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Bootstrap.php')) {
 
             \yongtiger\theme\Bootstrap::filterExtensionsBootstrap();
